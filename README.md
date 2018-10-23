645fe3af962a393f09b9a4bae4ffa4a569c4e4cb
Jacob Bearce <jacob@bearce.me>
2018-10-23 11:19:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# iOS Install Prompt
+
+Prompt iOS users to add your PWA to their home screen, since Apple won’t.
+
+## Installation
+
+### via npm
+
+```sh
+npm install ios-install-prompt --save
+```
+
+### via yarn
+
+```sh
+yarn add ios-install-prompt
+```
+
+## Usage
+
+```js
+import iosInstallPrompt from "ios-install-prompt";
+new iosInstallPrompt(".ios-install-prompt__container");
+```
+
+## Default Markup
+
+```html
+<div class="ios-install-prompt__container">
+    <button class="ios-install-prompt__overlay">Close</button>
+    <div class="ios-install-prompt">
+        <img class="ios-install-prompt__icon" src="/path/to/icon/file.png" alt="{app_name}" />
+        <div class="ios-install-prompt__content">
+            <h3 class="ios-install-prompt__title">Install {app_name}</h3>
+            <p class="ios-install-prompt__text">Install this application on your home screen for quick and easy access when you’re on the go.</p>
+            <p class="ios-install-prompt__guide">Just tap <svg class="ios-install-prompt__guide__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000"><path fill="#1A84FF" d="M381.9,181l95.8-95.8v525.9c0,13.4,8.9,22.3,22.3,22.3c13.4,0,22.3-8.9,22.3-22.3V85.2l95.8,95.8c4.5,4.5,8.9,6.7,15.6,6.7c6.7,0,11.1-2.2,15.6-6.7c8.9-8.9,8.9-22.3,0-31.2L515.6,16.1c-2.2-2.2-4.5-4.5-6.7-4.5c-4.5-2.2-11.1-2.2-17.8,0c-2.2,2.2-4.5,2.2-6.7,4.5L350.7,149.8c-8.9,8.9-8.9,22.3,0,31.2C359.6,190,373,190,381.9,181z M812,276.9H633.7v44.6H812v624H188v-624h178.3v-44.6H188c-24.5,0-44.6,20.1-44.6,44.6v624c0,24.5,20.1,44.6,44.6,44.6h624c24.5,0,44.6-20.1,44.6-44.6v-624C856.6,296.9,836.5,276.9,812,276.9z" /></svg> then “Add to Home Screen”</p>
+        </div>
+        <button class="ios-install-prompt__close">Close</button>
+    </div>
+</div>
+```
+
+## Options
+
+| Name           | Default                                 | Description                                            |
+|----------------|-----------------------------------------|--------------------------------------------------------|
+| `buttons`      | `.ios-install-prompt__container button` | Query selector to target to handle closing the prompt  |
+| `active_class` | `is-active`                             | Class name to be added when the prompt is visible      |
+| `show_after`   | `14`                                    | Number of days to wait before showing the prompt again |
---
+++ script.js
@@ -0,0 +1,73 @@
+/**
+ * iOS Install Prompt 0.1.0
+ * Prompt iOS users to add your PWA to their home screen, since Apple won’t.
+ * https://github.com/JacobDB/ios-install-prompt/
+ *
+ * Copyright 2018 Jacob Bearce
+ *
+ * Released under the MIT License
+ *
+ * Released on: October 23, 2018
+ */
+
+(function (global, factory) {
+    typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() :
+    typeof define === "function" && define.amd ? define(factory) :
+    (global.iosInstallPrompt = factory());
+}(this, (function () {
+    const DEFAULTS = {
+        container:    ".ios-install-prompt__container",
+        buttons:      ".ios-install-prompt__container button",
+        active_class: "is-active",
+        show_after:   14,
+        expires:      28,
+    };
+
+    const NEEDS_TO_SEE_PROMPT = (options) => {
+        // never show it if the app was launched standalone
+        if (navigator.standalone) {
+            return false;
+        }
+
+        // get the current date and the date the user last visited
+        const NOW  = Date.now();
+        const THEN = document.cookie.match(/last_visit=([0-9]+)/);
+
+        // update the last_visit cookie with Date.now()
+        document.cookie = "last_visit=" + NOW + "; max-age=" + (60 * 60 * 24 * options.expires);
+
+        // get the number of days since the user last visited and check if they're using iOS
+        const DAYS = THEN && THEN.length >= 2 ? Math.ceil((parseInt(THEN[1]) - NOW) / (1000 * 60 * 60 * 24)) : NaN;
+        const IOS  = ["iPad", "iPhone", "iPod"].includes(navigator.platform);
+
+        // show if the cookie isn't set, or last visit is > show_after, and it's iOS
+        return (isNaN(DAYS) || DAYS > options.show_after) && IOS;
+    };
+
+    const INSTALL_PROMPT = (function (container = DEFAULTS.container, options = {}) {
+        options.container    = container;
+        options.buttons      = options.buttons      || DEFAULTS.buttons;
+        options.active_class = options.active_class || DEFAULTS.active_class;
+        options.show_after   = options.show_after   || DEFAULTS.show_after;
+        options.expires      = options.expires      || DEFAULTS.expires;
+
+        const CONTAINER = document.querySelector(options.container);
+        const BUTTONS   = document.querySelectorAll(options.buttons);
+
+        if (NEEDS_TO_SEE_PROMPT(options)) {
+            if (CONTAINER) {
+                CONTAINER.classList.add(options.active_class);
+            }
+
+            if (BUTTONS.length) {
+                BUTTONS.forEach((button) => {
+                    button.addEventListener("click", () => {
+                        CONTAINER.classList.remove(options.active_class);
+                    })
+                });
+            }
+        }
+    });
+
+    return INSTALL_PROMPT;
+})));
