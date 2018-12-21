fe32f7c494fc7e16ea7370844d29b7c04a136428
Jacob Bearce <jacob@bearce.me>
2018-12-21 10:05:50 -0600

    Change cookie name to `pwa_install_prompt_last_visit` to prevent conflicts; Close #8

--- script.js
+++ script.js
@@ -37,10 +37,10 @@
 
         // get the current date and the date the user last visited
         const NOW  = Date.now();
-        const THEN = document.cookie.match(/last_visit=([0-9]+)/);
+        const THEN = document.cookie.match(/pwa_install_prompt_last_visit=([0-9]+)/);
 
-        // update the last_visit cookie with Date.now()
-        document.cookie = "last_visit=" + NOW + "; max-age=" + (60 * 60 * 24 * options.expires);
+        // update the pwa_install_prompt_last_visit cookie with Date.now()
+        document.cookie = "pwa_install_prompt_last_visit=" + NOW + "; max-age=" + (60 * 60 * 24 * options.expires);
 
         // get the number of days since the user last visited and check if they're using iOS
         const DAYS = THEN && THEN.length >= 2 ? Math.ceil((parseInt(THEN[1]) - NOW) / (1000 * 60 * 60 * 24)) : NaN;
